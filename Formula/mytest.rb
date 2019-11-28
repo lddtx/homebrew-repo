46f0356c3047e30a2b3b0af7531e3493d9875544
xiaopdd <1445101933@qq.com>
2019-11-29 02:14:36 +0800

    Update mytest.rb

--- Formula/mytest.rb
+++ Formula/mytest.rb
@@ -1,9 +1,9 @@
 class Mytest < Formula
   desc "mytest"
   homepage ""
-  url ""
+  url "https://repo1.maven.org/maven2/com/alibaba/fastjson/1.2.58/fastjson-1.2.58.jar"
   version "0.0.1"
-  sha256 ""
+  sha256 "4a25dfcec212ab1b5e31fd6c0ea68272a89bb2e5c5c37deb1a3a9a8172497409"
   
   def install
     (bin/"mytest").write <<~EOS
