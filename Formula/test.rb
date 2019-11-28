3eb7cd59c28b082b238b094c3086c469165dedd5
xiaopdd <1445101933@qq.com>
2019-11-29 01:43:04 +0800

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,7 +1,7 @@
 class Test < Formula
   desc "GUI tool for investigating Java class files"
   homepage "https://github.com/zxh0/classpy"
-  url "https://github.com/guxingke/oss/raw/master/dist/classpy/classpy-0.7.0.jar"
+  url "https://repo1.maven.org/maven2/com/alibaba/fastjson/1.2.58/fastjson-1.2.58.jar"
   version "0.7.0"
   sha256 "7155743c22e93f9adbbae0e8fcd3de70ce852f26ac1558e819a63109b770e033"
 
@@ -10,9 +10,10 @@ class Test < Formula
   depends_on :java
 
   def install
+    prefix.install "fastjson-1.2.58.jar"
     (bin/"mytest").write <<~EOS
       #!/usr/bin/env bash
-      exec echo "just for test"
+      echo "just for test, hello world"
     EOS
   end
 end
