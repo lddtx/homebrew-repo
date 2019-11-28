33188d2b4040ce6efe45169c3468247d2ec5b02a
xiaopdd <1445101933@qq.com>
2019-11-29 02:08:03 +0800

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -4,8 +4,12 @@ class Test < Formula
   url "https://repo1.maven.org/maven2/com/alibaba/fastjson/1.2.58/fastjson-1.2.58.jar"
   version "0.7.0"
   sha256 "4a25dfcec212ab1b5e31fd6c0ea68272a89bb2e5c5c37deb1a3a9a8172497409"
-
+  depends_on :java
+  
   def install
-    shell_output(111111111111)
+    (bin/"test").write <<~EOS
+      #!/usr/bin/env bash
+      exec java -version
+    EOS
   end
 end
