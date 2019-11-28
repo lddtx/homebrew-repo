f06420813017b9088271e69ee1b3ea31f7ced646
xiaopdd <1445101933@qq.com>
2019-11-29 01:44:43 +0800

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -3,7 +3,7 @@ class Test < Formula
   homepage "https://github.com/zxh0/classpy"
   url "https://repo1.maven.org/maven2/com/alibaba/fastjson/1.2.58/fastjson-1.2.58.jar"
   version "0.7.0"
-  sha256 "7155743c22e93f9adbbae0e8fcd3de70ce852f26ac1558e819a63109b770e033"
+  sha256 "4a25dfcec212ab1b5e31fd6c0ea68272a89bb2e5c5c37deb1a3a9a8172497409"
 
   bottle :unneeded
 
